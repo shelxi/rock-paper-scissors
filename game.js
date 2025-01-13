a069b5cdd57e8ed743e132aedd328187eda8b0c9
shelxi <189994946+shelxi@users.noreply.github.com>
2025-01-13 18:35:00 +0100

    Added function for computer choice
    logged to console to confirm code is working

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+
+function getComputerChoice() {
+    const randomNum = 
+Math.floor(Math.random() * 3);
+    if(randomNum === 0) {
+        return "rock";
+    } else if(randomNum === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
+
