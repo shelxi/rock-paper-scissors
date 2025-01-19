4776a0cfb88385829ff6ecc7dd8702bbe51d1aee
shelxi <189994946+shelxi@users.noreply.github.com>
2025-01-19 16:07:36 +0100

    Added playRound function to game.js
    Also Added playGame to call and play the game.

--- game.js
+++ game.js
@@ -23,5 +23,45 @@ prompt("choose a number", "");
     }
 }
 
-console.log(getHumanChoice());
 
+function playGame() {
+
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    
+    humanChoice = humanChoice.toLowerCase();
+    
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie! You both chose ${humanChoice}.`);
+        return;
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+              (humanChoice === "paper" && computerChoice === "rock") ||
+              (humanChoice === "scissors" && computerChoice === "paper")) {
+
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+                humanChoice++;
+              } else {
+                console.log(`You lose! ${computerChoice} beat ${humanChoice}.`);
+                computerChoice++;
+              }
+
+}
+
+//Get the human and computer choices
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+playRound(humanSelection, computerSelection);
+
+
+}
+
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
